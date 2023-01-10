0a49b9ec65f12da10ce9c0b23be6a169f84c66f1
sink <lego9182@gmail.com>
2023-01-10 20:29:53 +0000

    improved database stuff

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -4,16 +4,20 @@ module.exports = {
 	name: "messageCreate",
 	async execute(message) {
 		console.log(`a message was created: ${message} by ${message.author} in ${message.guild}`)
+		guild = message.guild
+		user = message.author
 		//console.log(message)
 		// add xp
 		//path = `$.${message.guild.id.toString()}.user.${message.author.id}.xp`
 
-		await database.check(`.${message.guild.id}.users.${message.author.id}`)
+		//await database.check(`.${message.guild.id}.users.${message.author.id}`)
 
-		path = `.${message.guild.id}.users.${message.author.id}`
 		curXp = await database.get(path + `.xp`) || 0
 		//console.log(`c: ${JSON.stringify(curXp)}`)
 		newXp = Math.floor(curXp + 5 + Math.random() * 10)
-		database.set(path + `.xp`, newXp)
+
+		path = `.${message.guild.id}.users.${message.author.id}`
+
+		await database.set(`.users.${user.id}.guilds.${guild.id}.xp`, newXp)
 	},
 }

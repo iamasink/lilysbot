13259fbb4e8a7c397c8de7ea3c477ef68b6a560a
iamasink <iamasink@proton.me>
2023-02-15 12:06:50 +0000

    invite tracking

--- src/events/inviteCreate.ts
+++ src/events/inviteCreate.ts
@@ -1,16 +1,17 @@
-const database = require("../structure/database")
-
+import { Events, Interaction, Invite, Message } from "discord.js"
+import Event from "../types/Event"
+import { client } from "../index"
+import database from "../utils/database"
 
 // Emitted whenever an invite is created
-
-module.exports = {
-	name: "inviteCreate",
-	async execute(invite) {
+export default new Event({
+	name: Events.InviteCreate,
+	async execute(invite: Invite) {
 		console.log(`a invite was created: ${invite}`)
 		console.log(invite)
 		database.set(`.guilds.${invite.guild.id}.invites.${invite.code}`, {
 			inviterId: invite.inviterId, uses: invite.uses, expired: false, code: invite.code
 		})
-
 	},
 }
+)
---
+++ src/events/inviteDelete.ts
@@ -0,0 +1,16 @@
+import { Events, Interaction, Invite, Message } from "discord.js"
+import Event from "../types/Event"
+import { client } from "../index"
+import database from "../utils/database"
+
+// Emitted whenever an invite is created
+export default new Event({
+	name: Events.InviteDelete,
+	async execute(invite: Invite) {
+		console.log(`a invite was deleted: ${invite}`)
+		console.log(invite)
+		database.set(`.guilds.${invite.guild.id}.invites.${invite.code}.expired`, true)
+
+	},
+}
+)

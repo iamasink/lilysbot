4ae4acd272be29f1bc90a6a941fda461aacf1c09
iamasink <iamasink@proton.me>
2023-05-10 01:58:10 +0000

    context menyu info command

---
+++ src/commands/info-contextmenu.ts
@@ -0,0 +1,38 @@
+import { Interaction, ChatInputCommandInteraction, GuildMember, SlashCommandBuilder, SlashCommandSubcommandBuilder, EmbedBuilder, Embed, ContextMenuCommandBuilder, ContextMenuCommandType, ApplicationCommandType, Guild } from "discord.js"
+import database from "../utils/database"
+import format from "../utils/format"
+import ApplicationCommand from "../types/ApplicationCommand"
+import { client } from ".."
+import embeds from "../utils/embeds"
+import axios from "axios"
+import { stripIndent } from "common-tags"
+import commands from "../utils/commands"
+
+
+
+
+
+export default new ApplicationCommand({
+	data: new ContextMenuCommandBuilder()
+		.setName("User Info")
+		.setType(ApplicationCommandType.User)
+	,
+	async menu(interaction) {
+		if (!interaction.guild) {
+			interaction.reply({ embeds: embeds.warningEmbed("This isn't available in DMs.") })
+			return
+		}
+
+		const member = (interaction.member as GuildMember)
+
+		await commands.run(interaction, "slash", "info", null, "user", [
+			{
+				name: 'target',
+				type: 6,
+				value: member.id,
+				user: await client.users.fetch(member.id),
+				member: await interaction.guild.members.fetch(member.id)
+			}
+		])
+	}
+})

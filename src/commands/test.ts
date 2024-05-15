51e0cb16e96fa9c0106d28adbc02b6c807cb93c1
iamasink <iamasink@proton.me>
2024-05-15 23:49:59 +0100

    test

--- src/commands/test.ts
+++ src/commands/test.ts
@@ -1,7 +1,9 @@
-import { ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } from 'discord.js'
+import { ActionRowBuilder, ChannelSelectMenuBuilder, ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } from 'discord.js'
 import ApplicationCommand from '../types/ApplicationCommand'
 import { client } from '..'
 import format from '../utils/format'
+import database from '../utils/database'
+import moment, { now } from 'moment'
 
 export default new ApplicationCommand({
 	permissions: ["Administrator"],
@@ -9,15 +11,8 @@ export default new ApplicationCommand({
 		.setName('test')
 		.setDescription('description'),
 	async execute(interaction: ChatInputCommandInteraction): Promise<void> {
+		const info = database.set(".info2", `data ${moment().format()}`)
 
-		const string = `aaaa\\_bbbb\\_cccc aaaa\\_bbbb\\_ aaaa \\_\\_underline\\_\\_\\_ this\\_\\_should\\_\\_ be underlined this\\_shouldnt\\_be\\_italicized\\_ except at the end!!! and just a normal _italics_ i love ~~strikethroughs~~ **lol*** a***`
-		console.log(string)
-
-
-
-
-
-
-		interaction.reply(`\`\`\`${format.markdownEscape(string)}\`\`\``)
 	},
 })
+

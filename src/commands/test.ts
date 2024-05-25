aa8199781ad49b1716279be8b02ea93145d90067
iamasink <iamasink@proton.me>
2024-05-25 16:58:31 +0100

    test

--- src/commands/test.ts
+++ src/commands/test.ts
@@ -1,4 +1,4 @@
-import { ActionRowBuilder, ChannelSelectMenuBuilder, ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } from 'discord.js'
+import { ActionRowBuilder, ChannelSelectMenuBuilder, ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder, TextBasedChannel } from 'discord.js'
 import ApplicationCommand from '../types/ApplicationCommand'
 import { client } from '..'
 import format from '../utils/format'
@@ -6,14 +6,12 @@ import database from '../utils/database'
 import moment, { now } from 'moment'
 
 export default new ApplicationCommand({
-	permissions: ["Administrator"],
+	permissions: ["botowner"],
 	data: new SlashCommandBuilder()
 		.setName('test')
 		.setDescription('description'),
 	async execute(interaction: ChatInputCommandInteraction): Promise<void> {
-		const info = database.set(".info2", `data ${moment().format()}`)
 
-		console.log(client.commands.get("settings"))
 	},
 })
 

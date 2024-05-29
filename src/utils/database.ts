a1716f002f86c746a573063f9e2b7b430f20b78a
iamasink <iamasink@proton.me>
2024-05-29 23:17:54 +0100

    improve database caught error handling, fixes #55

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -33,6 +33,7 @@ async function set(path: string, data: any) {
 
 	// try and set the data.
 	try {
+		// console.log(`redis setting ${path} = ${JSON.stringify(data)}`)
 		let res = await db.json.set(key, path, data)
 		//console.log(`redis response: ${res}`)
 		if (!res) throw new Error("null")
@@ -40,27 +41,29 @@ async function set(path: string, data: any) {
 	}
 	catch (e) {
 		let newpath
-		console.log("s" + e.toString())
-		console.log("n" + e.name)
-		console.log("m" + e.message)
-		console.log("c" + e.cause)
+		// console.log("s" + e.toString())
+		// console.log("n" + e.name)
+		// console.log("m" + e.message)
+		// console.log("c" + e.cause)
 
-		console.log(path)
-		console.log(data)
+		// console.log(`redis set caught error: ${path} = ${JSON.stringify(data)}`)
 		switch (e.message) {
-			case 'ERR new objects must be created at the root': { // what was this for?
-				console.log("error root")
+			case 'ERR new objects must be created at the root': {
+				// i think this happens when there is no key set, so set it?
+				// console.log("error root")
 				newpath = path.split(".")
 				newpath.pop()
-				await this.set("." + newpath, {})
+				await set(".", {}) // setup the root
+				await set(path, data) // try again with this value
 				break
 			}
 			case 'null': { // if it failed to set the value, fix it idk
-				console.log("error null")
+				// console.log("error null")
 				newpath = path.split(".")
+				// console.log("newpath" + newpath)
 				newpath.pop()
-				await set(newpath.join("."), {}) // go up one layer and set the json stuffs
-				await set(path, data) // try again so its recursive ?? it seemed to work so idk
+				await set(newpath.join("."), {}) // go up one layer and initalize the parent object
+				await set(path, data) // try again with this value
 				break
 			}
 			default: {
